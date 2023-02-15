28be840f2d59d394062e7a92fafd8725eaf8a274
Eoin Darcy <eoindarcy@sidero.ie>
2023-02-15 16:49:56 +0000

    Initial Commit

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const dotenv = require("dotenv").config();
+
+// Initialise Express
+const app = express();
+
+mongoose.set("strictQuery", true);
+
+// Establish Connection to MongoDB OR Use Port 5000
+const port = process.env.port || 5000;
+
+// Connect to MongoDB and Start Server
+mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => {
+        app.listen(port, () => {
+            console.log(`Server Running on Port ${port}`);
+        })
+    })
+    .catch((err) => console.log(err))
